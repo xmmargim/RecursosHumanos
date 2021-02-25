bc7d52686d59689b2a8c18031f182496402421b1
xmmargim <xmmargim359@ieshnosmachado.org>
2021-02-25 13:02:42 +0100

    Subiendo Proyecto Terminado

--- app/src/main/java/com/example/recursoshumanos/adapterEmpleado.java
+++ app/src/main/java/com/example/recursoshumanos/adapterEmpleado.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
@@ -12,6 +14,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class adapterEmpleado extends FirestoreRecyclerAdapter<Empleado,
         adapterEmpleado.ViewHolder> {
@@ -38,12 +43,29 @@ public class adapterEmpleado extends FirestoreRecyclerAdapter<Empleado,
                                     @NonNull Empleado empleado) {
         holder.mNombre.setText(empleado.nombre);
         holder.mApellidos.setText(empleado.apellidos);
+        holder.mBorrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseFirestore mFirebaseFireStore= FirebaseFirestore.getInstance();
+                DocumentSnapshot lugarDocument =
+                        getSnapshots().getSnapshot(holder.getAdapterPosition());
+                mFirebaseFireStore.collection("empleados").document(lugarDocument.getId())
+                    .delete()
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(frAct, "Empleado Borrado", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
         public final TextView mNombre;
         public final TextView mApellidos;
+        public final Button mBorrar;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -51,6 +73,7 @@ public class adapterEmpleado extends FirestoreRecyclerAdapter<Empleado,
             mView = itemView;
             mNombre = (TextView) mView.findViewById(R.id.txtNombre);
             mApellidos = (TextView) mView.findViewById(R.id.txtResidente);
+            mBorrar = (Button) mView.findViewById(R.id.btnBorrar);
 
         }
     }
